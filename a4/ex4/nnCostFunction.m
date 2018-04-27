fa3b84cb85c108e300331c6df9a7eecf721a71fc
ZerkTheMighty <codyrosevear@Codys-Air.hitronhub.home>
2018-04-27 00:31:59 -0600

    Replace old backpropr code with vectorized version in assignment 4

--- a4/ex4/nnCostFunction.m
+++ a4/ex4/nnCostFunction.m
@@ -8,8 +8,8 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
 %   X, y, lambda) computes the cost and gradient of the neural network. The
 %   parameters for the neural network are "unrolled" into the vector
-%   nn_params and need to be converted back into the weight matrices. 
-% 
+%   nn_params and need to be converted back into the weight matrices.
+%
 %   The returned parameter grad should be a "unrolled" vector of the
 %   partial derivatives of the neural network.
 %
@@ -24,8 +24,8 @@ Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):en
 
 % Setup some useful variables
 m = size(X, 1);
-         
-% You need to return the following variables correctly 
+
+% You need to return the following variables correctly
 J = 0;
 Theta1_grad = zeros(size(Theta1));
 Theta2_grad = zeros(size(Theta2));
@@ -46,12 +46,12 @@ Theta2_grad = zeros(size(Theta2));
 %         that your implementation is correct by running checkNNGradients
 %
 %         Note: The vector y passed into the function is a vector of labels
-%               containing values from 1..K. You need to map this vector into a 
+%               containing values from 1..K. You need to map this vector into a
 %               binary vector of 1's and 0's to be used with the neural network
 %               cost function.
 %
 %         Hint: We recommend implementing backpropagation using a for-loop
-%               over the training examples if you are implementing it for the 
+%               over the training examples if you are implementing it for the
 %               first time.
 
 % Part 3: Implement regularization with the cost function and gradients.
@@ -62,88 +62,60 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-for i = 1:m
-    %Get the current example and add the bias units
-    a1 = [1 X(i, :)];
-   
-    %Convert the output label into a corresponding bit vector
-    curExOutputLabel = y(i, :);
-    curExOutputVec = zeros(num_labels, 1);
-    curExOutputVec(curExOutputLabel) = 1;
-    
-    %Compute the hidden layer
-    z1 = a1 * Theta1';
-    a2 = sigmoid(z1);
-    
-    %Add the bias unit the the hidden layer (as a row)
-    a2 = [1 a2];
-    z2 = a2 * Theta2';
-    a3 = sigmoid(z2);
-  
-    %Backpropogate
-    delta3 = a3 - curExOutputVec';
-    delta3
-    %Remove the weights for the hidden layer bias unit: since it does not
-    %connect to anything in the input layer, we don't backpropogate on it
+%VECTORIZED METHOD
+%Modify the target matrix so that it is a matrix of 1 hot encoded vectors
+eye_matrix = eye(num_labels);
+y_matrix = eye_matrix(y,:);
 
-      size(delta3)
-      size(Theta2)
-      size(sigmoidGradient(z2))
-      delta2 = delta3 * Theta2 .* sigmoidGradient(z2)';
-      delta2 = delta2(:, 2:end);     
-      delta2
-    
-    %Compute the gradients
-%     temp1 = delta2 * a1';
-%     temp2 = delta3 * a2';
-%     Theta1_grad = Theta1_grad + temp1;
-%     Theta2_grad = Theta2_grad + temp2;
-     
-    %Compute the cost over all of the output units for the current example
-    hypothesis = a3;
-    for j = 1:num_labels
-       curCost = (-curExOutputVec(j) .* log(hypothesis(j))) - ((1 - curExOutputVec(j)) .* log(1 - hypothesis(j)));
-       J = J + curCost;
-    end
-end
-J = J ./ m;
+%Forwardpropagate
+%Get the current example, add the bias units, and compute the hidden layer activation
+a1 = [ones(size(X, 1), 1) X];
+z2 = a1 * transpose(Theta1);
+a2 = sigmoid(z2);
 
-%Compute the final partial derivatives used by the optimization algorithm
-Theta1_grad = Theta1_grad ./ m;
-Theta2_grad = Theta2_grad ./ m;
+%Add the bias unit to the hidden layer and compute the final output
+a2 = [ones(size(a2, 1), 1) a2];
+z3 = a2 * transpose(Theta2);
+a3 = sigmoid(z3);
 
-%Regularize the cost function
-regCost = 0;
-params = {Theta1, Theta2};
-for i = 1:length(params)
-    curTheta = params{i};
-    unBiasedTheta = curTheta(:, 2:end);
-    for j = 1:size(unBiasedTheta, 1)
-        for k = 1:size(unBiasedTheta, 2)
-            regCost = regCost + (unBiasedTheta(j, k) ^ 2);
-        end
-    end
-end
+%Backpropagate
+%Compute the error for the output layer
+d3 = a3 - y_matrix;
 
-regCost = (lambda ./ (2 .* m)) .* regCost;
-J = J + regCost;
+%Compute the error for the hidden layer
+d2 = (d3 * Theta2(:, 2:end)) .* sigmoidGradient(z2);
 
+%Compute the vector of partial derivatives (gradient) for each layer
+delta_2 = transpose(d3) * a2;
+delta_1 = transpose(d2) * a1;
 
+%Scale the gradients
+Theta1_grad = delta_1 ./ m;
+Theta2_grad = delta_2 ./ m;
 
+%Compute the unregularized cost
+cost_term_1 = transpose(-y_matrix) * log(a3);
+cost_term_2 = transpose(1 - y_matrix) * log(1 - a3);
+%We have to multiple by the identity matrix to eliminate the non diagonal
+%terms, since they are not relevant to the cost function when y and h are
+%matrices and not vectors
+J = (sum(sum(eye_matrix .* cost_term_1)) - sum(sum((eye_matrix .* cost_term_2)))) ./ m
 
+%Compute the regularized cost
+regCost = (lambda ./ (2 .* m)) .* (sum(sum(Theta1(:, 2:end) .^ 2)) + sum(sum(Theta2(:, 2:end) .^ 2)));
 
+%Total cost
+J = J + regCost
 
+%Modify the gradients to take into account the regularization
+%We can modify the weight matrices to exclude the bias units, since we've already computed h(x)
+Theta1(:,1) = 0;
+Theta2(:,1) = 0;
+Theta1 = Theta1 .* (lambda ./ m);
+Theta2 = Theta2 .* (lambda ./ m);
 
-
-
-
-
-
-
-
-
-
-
+Theta1_grad = Theta1_grad + Theta1;
+Theta2_grad = Theta2_grad + Theta2;
 
 % -------------------------------------------------------------
 
